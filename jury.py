6e1354a4a42cd29baf4f4e4167b20d8cfb30dbdf
Vinamr Sachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-12-10 00:00:09 +0530

    Update jury.py

--- jury.py
+++ jury.py
@@ -4,7 +4,6 @@ def correctness(p, n):
     r = (n // 2) + 1
     P = 0
     while r <= n:
-        p_ = (comb(n, r) * (p**r) * ((1 - p)**(n-r)))
-        P += p_
+        P += (comb(n, r) * (p**r) * ((1 - p)**(n-r)))
         r += 1
     return P
