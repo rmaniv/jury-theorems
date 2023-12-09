95e6aa20bf72300dd0572a02a833c8e9d84085f3
vinamrsachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-12-10 00:29:25 +0530

    added super-majority and unanimity

--- jury.py
+++ jury.py
@@ -1,7 +1,15 @@
-from math import comb
+from math import ceil, comb
 
-def correctness(p, n):
-    r = (n // 2) + 1
+def correctness(p, n, m):
+    # simple majority
+    if m == 0:
+        r = (n // 2) + 1
+    # super majority or unanimity (for ex, m should be 75 if prefered super-majority level is 75%)
+    elif m > 50 and m <= 100:
+        r = ceil((m/100) * n)
+    else:
+        print("correctness(p, n, m) where m = 0 for simple-majority but > 50 and <= 100 for super-majority or unanimity")
+        return None
     P = 0
     while r <= n:
         P += (comb(n, r) * (p**r) * ((1 - p)**(n-r)))
--- theorems.py
+++ theorems.py
@@ -8,7 +8,7 @@ def condorcet(n_values):
     plt.figure(figsize=(10, 6))
     p_values = np.arange(0, 1.01, 0.01)
     for n in n_values:
-        y_values = [jury.correctness(p, n) for p in p_values]
+        y_values = [jury.correctness(p, n, 0) for p in p_values]
         plt.plot(p_values, y_values, label=f'n={n}', color=np.random.rand(3,))
 
     plt.title('Probability of Correct Majority Decision by p and n')
