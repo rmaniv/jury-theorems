df660f8c26b1e3206b58fe2aa54accf2dc71dc56
vinamrsachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-12-10 02:26:28 +0530

    Update jury.py

--- jury.py
+++ jury.py
@@ -1,17 +1,11 @@
 from math import ceil, comb
 
 def correctness(p, n, m):
-    # simple majority
-    if m == 0:
-        r = (n // 2) + 1
-    # super majority or unanimity (for ex, m should be 75 if prefered super-majority level is 75%)
-    elif m > 50 and m <= 100:
-        r = ceil((m/100) * n)
-    else:
-        print("correctness(p, n, m) where m = 0 for simple-majority but > 50 and <= 100 for super-majority or unanimity")
-        return None
+    r = (n // 2) + 1
     P = 0
     while r <= n:
         P += (comb(n, r) * (p**r) * ((1 - p)**(n-r)))
         r += 1
+    if n % 2 == 0:
+        P += (comb(n, n//2) * (p**(n//2)) * ((1 - p)**(n-(n//2)))) # with coin flip ties
     return P
