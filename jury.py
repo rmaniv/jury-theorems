0c221438d8a4ba6b15c959d31a5a2574e34c9e3e
vinamrsachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-12-10 17:41:39 +0530

    monte carlo simulation based implementation

--- jury.py
+++ jury.py
@@ -1,11 +1,19 @@
 from math import ceil, comb
+import random
 
-def correctness(p, n, m):
-    r = (n // 2) + 1
-    P = 0
-    while r <= n:
-        P += (comb(n, r) * (p**r) * ((1 - p)**(n-r)))
-        r += 1
-    if n % 2 == 0:
-        P += (comb(n, n//2) * (p**(n//2)) * ((1 - p)**(n-(n//2)))) # with coin flip ties
-    return P
+def correctness(p, n, m, num_trials=int(1e6)):
+    if m == 0:
+        required_majority = (n // 2) + 1
+    elif 50 < m <= 100:
+        required_majority = ceil((m / 100) * n)
+    else:
+        print("correctness_simulation(p, n, m) where m = 0 for simple-majority but > 50 and <= 100 for super-majority or unanimity")
+        return None
+
+    successful_trials = 0
+    for _ in range(num_trials):
+        votes = sum(random.random() < p for _ in range(n))
+        if votes >= required_majority:
+            successful_trials += 1
+
+    return successful_trials / num_trials
