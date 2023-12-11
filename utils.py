ba596bd73aebfb8ca54fd12167f09874042e9ff7
vinamrsachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-12-11 23:02:07 +0530

    added even checker in utils.majority and removed redundancy in condorcet.deterministic

--- utils.py
+++ utils.py
@@ -5,7 +5,10 @@ import numpy as np
 
 def majority(n, m=0):
     if m == 0:
-        r = (n // 2) + 1
+        if n % 2:
+            r = (n // 2) + 1
+        else:
+            r = n // 2
     elif m > 50 and m <= 100: r = ceil((m / 100) * n)
     else:
         print("correctness_simulation(p, n, m) where m = 0 for simple-majority but > 50 and <= 100 for super-majority or unanimity")
